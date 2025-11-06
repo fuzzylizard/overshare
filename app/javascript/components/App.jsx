b49ab97a50138fb4f8ccae6bc8a8712b3ccacb8e
Christopher Johnston <chris@fuzzylizard.com>
2025-11-06 14:53:28 -0500

    formatting updates, put key in right place for posts

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Posts from "./Posts";
 
 const App = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold">Hello from react</h1>
-            <Posts />
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Hello from react</h1>
+      <Posts />
+    </div>
+  );
 };
 
 export default App;
--- app/javascript/components/Posts.jsx
+++ app/javascript/components/Posts.jsx
@@ -1,43 +1,40 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 function Posts() {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    async function getData() {
-        const url = "http://127.0.0.1:3000/posts";
-        try {
-            const response = await fetch(url, {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-            }
+  async function getData() {
+    const url = "http://127.0.0.1:3000/posts";
+    try {
+      const response = await fetch(url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
 
-            const result = await response.json();
-            console.log(result);
-            setPosts(result);
-        } catch (error) {
-            console.error(error.message);
-        }
+      const result = await response.json();
+      setPosts(result);
+    } catch (error) {
+      console.error(error.message);
     }
+  }
 
-    useEffect(() => {
-        getData();
-    }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-    return (
-        <div>
-            {posts.map((post) =>
-                (
-                    <div className="border-1 p-4 rounded-lg my-5">
-                        <p key={post.id}>{post.message}</p>
-                    </div>
-                )
-            )}
+  return (
+    <div>
+      {posts.map((post) => (
+        <div className="border-1 p-4 rounded-lg my-5" key={post.id}>
+          <p>{post.message}</p>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default Posts;
